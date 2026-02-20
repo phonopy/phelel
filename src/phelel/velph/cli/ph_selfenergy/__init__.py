6ff186df3115c054a01ca054cb0d1a637fc73dde
Atsushi Togo <atz.togo@gmail.com>
2026-02-20 11:21:18 +0900

    Avoid writing many lines in __init__.py in velph ph_selfenergy

--- src/phelel/velph/cli/ph_selfenergy/__init__.py
+++ src/phelel/velph/cli/ph_selfenergy/__init__.py
@@ -1,56 +1,10 @@
 """velph command line tool / velph-ph_selfenergy."""
 
-import pathlib
-
-import click
-
-from phelel.velph.cli import cmd_root
-from phelel.velph.cli.selfenergy.generate import write_selfenergy_input_files
-from phelel.velph.cli.utils import check_fft
-
-
-@cmd_root.group("ph_selfenergy")
-@click.help_option("-h", "--help")
-def cmd_ph_selfenergy():
-    """Choose ph_selfenergy options."""
-    pass
-
-
-@cmd_ph_selfenergy.command("generate")
-@click.argument(
-    "toml_filename",
-    nargs=1,
-    type=click.Path(),
-    default="velph.toml",
+from phelel.velph.cli.ph_selfenergy.cmd_ph_selfenergy import (
+    cmd_check_fft,  # noqa F401 : velph ph_selfenergy check-fft
+    cmd_generate,  # noqa F401 : velph ph_selfenergy generate
+    cmd_ph_selfenergy,  # noqa F401 : velph ph_selfenergy <subcommand>
 )
-@click.option(
-    "--dry-run/--no-dry-run",
-    "-d",
-    "dry_run",
-    is_flag=True,
-    default=False,
-    show_default=True,
+from phelel.velph.cli.ph_selfenergy.plot import (
+    cmd_plot,  # noqa F401 : velph ph_selfenergy plot <subcommand>
 )
-@click.help_option("-h", "--help")
-def cmd_generate(toml_filename: str, dry_run: bool):
-    """Generate ph_selfenergy input files."""
-    if not pathlib.Path("POTCAR").exists():
-        click.echo('"POTCAR" not found in current directory.')
-
-    write_selfenergy_input_files(pathlib.Path(toml_filename), dry_run, "ph_selfenergy")
-
-
-@cmd_ph_selfenergy.command("check-fft")
-@click.argument(
-    "toml_filename",
-    nargs=1,
-    type=click.Path(),
-    default="velph.toml",
-)
-@click.help_option("-h", "--help")
-def cmd_check_fft(toml_filename: str):
-    """Show [NGX, NGY, NGZ] in vasprun.xml."""
-    check_fft(toml_filename, "ph_selfenergy")
-
-
-from phelel.velph.cli.ph_selfenergy.plot import cmd_plot  # noqa F401
